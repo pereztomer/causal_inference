c78f06d0a475ed8af1b8666ac5adc5f1eec78393
pereztomer <pereztomer3@gmail.com>
2023-01-18 20:07:43 +0200

    asdasdasd

--- main.py
+++ main.py
@@ -1,66 +1,85 @@
+#
+# from nba_api.stats.static import teams
+# import pandas as pd
+#
+# def combine_team_games(df, keep_method='home'):
+#     '''Combine a TEAM_ID-GAME_ID unique table into rows by game. Slow.
+#
+#         Parameters
+#         ----------
+#         df : Input DataFrame.
+#         keep_method : {'home', 'away', 'winner', 'loser', ``None``}, default 'home'
+#             - 'home' : Keep rows where TEAM_A is the home team.
+#             - 'away' : Keep rows where TEAM_A is the away team.
+#             - 'winner' : Keep rows where TEAM_A is the losing team.
+#             - 'loser' : Keep rows where TEAM_A is the winning team.
+#             - ``None`` : Keep all rows. Will result in an output DataFrame the same
+#                 length as the input DataFrame.
+#
+#         Returns
+#         -------
+#         result : DataFrame
+#     '''
+#     # Join every row to all others with the same game ID.
+#     joined = pd.merge(df, df, suffixes=['_A', '_B'],
+#                       on=['SEASON_ID', 'GAME_ID', 'GAME_DATE'])
+#     # Filter out any row that is joined to itself.
+#     result = joined[joined.TEAM_ID_A != joined.TEAM_ID_B]
+#     # Take action based on the keep_method flag.
+#     if keep_method is None:
+#         # Return all the rows.
+#         pass
+#     elif keep_method.lower() == 'home':
+#         # Keep rows where TEAM_A is the home team.
+#         result = result[result.MATCHUP_A.str.contains(' vs. ')]
+#     elif keep_method.lower() == 'away':
+#         # Keep rows where TEAM_A is the away team.
+#         result = result[result.MATCHUP_A.str.contains(' @ ')]
+#     elif keep_method.lower() == 'winner':
+#         result = result[result.WL_A == 'W']
+#     elif keep_method.lower() == 'loser':
+#         result = result[result.WL_A == 'L']
+#     else:
+#         raise ValueError(f'Invalid keep_method: {keep_method}')
+#     return result
+#
+# nba_teams = teams.get_teams()
+# # Select the dictionary for the Celtics, which contains their team ID
+# celtics = [team for team in nba_teams if team['abbreviation'] == 'BOS'][0]
+# celtics_id = celtics['id']
+#
+# minisota = [team for team in nba_teams if team['abbreviation'] == 'MIN'][0]
+# minisota_id = minisota['id']
+#
+# from nba_api.stats.endpoints import leaguegamefinder
+#
+# # Query for games where the Celtics were playing
+# bos_gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=celtics_id)
+# minesota_gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=minisota_id)
+# # The first DataFrame of those returned is what we want.
+# bos_games = bos_gamefinder.get_data_frames()[0]
+# minesota_games = minesota_gamefinder.get_data_frames()[0]
+# games = pd.concat([bos_games, minesota_games])
+# print(games)
+#
+# game_df = combine_team_games(games)
+# print(game_df)
 
-from nba_api.stats.static import teams
+from nba_api.stats.endpoints import teamgamelog
 import pandas as pd
 
-def combine_team_games(df, keep_method='home'):
-    '''Combine a TEAM_ID-GAME_ID unique table into rows by game. Slow.
+# Set the season year
+season = '2021'
 
-        Parameters
-        ----------
-        df : Input DataFrame.
-        keep_method : {'home', 'away', 'winner', 'loser', ``None``}, default 'home'
-            - 'home' : Keep rows where TEAM_A is the home team.
-            - 'away' : Keep rows where TEAM_A is the away team.
-            - 'winner' : Keep rows where TEAM_A is the losing team.
-            - 'loser' : Keep rows where TEAM_A is the winning team.
-            - ``None`` : Keep all rows. Will result in an output DataFrame the same
-                length as the input DataFrame.
+# Get the team game logs for the specified season
+team_gamelogs = teamgamelog.TeamGameLog(team_id=1610612738)
 
-        Returns
-        -------
-        result : DataFrame
-    '''
-    # Join every row to all others with the same game ID.
-    joined = pd.merge(df, df, suffixes=['_A', '_B'],
-                      on=['SEASON_ID', 'GAME_ID', 'GAME_DATE'])
-    # Filter out any row that is joined to itself.
-    result = joined[joined.TEAM_ID_A != joined.TEAM_ID_B]
-    # Take action based on the keep_method flag.
-    if keep_method is None:
-        # Return all the rows.
-        pass
-    elif keep_method.lower() == 'home':
-        # Keep rows where TEAM_A is the home team.
-        result = result[result.MATCHUP_A.str.contains(' vs. ')]
-    elif keep_method.lower() == 'away':
-        # Keep rows where TEAM_A is the away team.
-        result = result[result.MATCHUP_A.str.contains(' @ ')]
-    elif keep_method.lower() == 'winner':
-        result = result[result.WL_A == 'W']
-    elif keep_method.lower() == 'loser':
-        result = result[result.WL_A == 'L']
-    else:
-        raise ValueError(f'Invalid keep_method: {keep_method}')
-    return result
+# Convert the results to a DataFrame
+team_gamelogs_df = pd.DataFrame(team_gamelogs.get_dict()['TeamGameLog'])
 
-nba_teams = teams.get_teams()
-# Select the dictionary for the Celtics, which contains their team ID
-celtics = [team for team in nba_teams if team['abbreviation'] == 'BOS'][0]
-celtics_id = celtics['id']
+# Filter the DataFrame to include only the columns you need
+team_gamelogs_df = team_gamelogs_df[['TEAM_NAME', 'TEAM_ID','RATING']]
 
-minisota = [team for team in nba_teams if team['abbreviation'] == 'MIN'][0]
-minisota_id = minisota['id']
+# Display the DataFrame
+print(team_gamelogs_df)
 
-from nba_api.stats.endpoints import leaguegamefinder
-
-# Query for games where the Celtics were playing
-bos_gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=celtics_id)
-minesota_gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=minisota_id)
-# The first DataFrame of those returned is what we want.
-bos_games = bos_gamefinder.get_data_frames()[0]
-minesota_games = minesota_gamefinder.get_data_frames()[0]
-games = pd.concat([bos_games, minesota_games])
-print(games)
-
-game_df = combine_team_games(games)
-print(game_df)
