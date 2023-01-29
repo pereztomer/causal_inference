f5c41b3f39899a94bb7934824f2324a5e4159cd4
ArielNov <97677462+ArielNov@users.noreply.github.com>
2023-01-29 15:14:53 +0200

    bla

--- pase_ariel.py
+++ pase_ariel.py
@@ -4,6 +4,18 @@ from nba_api.stats.endpoints import leaguegamefinder
 import numpy as np
 
 
+def get_pace_by_game_id_and_team_id(game_id, team_id):
+    out = boxscoreadvancedv2.BoxScoreAdvancedV2(game_id=game_id)
+
+    teams_id_2 = out.team_stats.data['data'][1][1]
+    location = 0
+    if teams_id_2 == team_id:
+        location = 1
+    pace = out.team_stats.data['data'][location][25]
+
+    return pace
+
+
 def generate_ds_pace():
     nba_teams = teams.get_teams()
     all_teams_id = [val['id'] for val in nba_teams]
@@ -13,7 +25,7 @@ def generate_ds_pace():
         # The first DataFrame of those returned is what we want.
         team_games = gamefinder.get_data_frames()[0]
         season_game_id_df = team_games[['SEASON_ID', 'GAME_ID', 'TEAM_NAME', 'TEAM_ID']]
-        season_game_id_df.loc[season_game_id_df.index, "PACE"] = np.nan
+        season_game_id_df.loc[:, "PACE"] = np.nan
         for index, s_g in season_game_id_df.iterrows():
             out = boxscoreadvancedv2.BoxScoreAdvancedV2(game_id=s_g['GAME_ID'])
 
