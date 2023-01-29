a3ce08c16a8ef9dcf197e292987b63ffe731a6c3
ArielNov <97677462+ArielNov@users.noreply.github.com>
2023-01-29 16:27:18 +0200

    bla

--- pase_ariel.py
+++ pase_ariel.py
@@ -2,7 +2,7 @@ from nba_api.stats.endpoints import boxscoreadvancedv2
 from nba_api.stats.static import teams
 from nba_api.stats.endpoints import leaguegamefinder
 import numpy as np
-
+import pandas as pd
 
 def get_pace_by_game_id_and_team_id(game_id, team_id):
     out = boxscoreadvancedv2.BoxScoreAdvancedV2(game_id=game_id)
@@ -12,7 +12,7 @@ def get_pace_by_game_id_and_team_id(game_id, team_id):
     if teams_id_2 == team_id:
         location = 1
     pace = out.team_stats.data['data'][location][25]
-
+    print(pace)
     return pace
 
 
@@ -25,22 +25,11 @@ def generate_ds_pace():
         # The first DataFrame of those returned is what we want.
         team_games = gamefinder.get_data_frames()[0]
         season_game_id_df = team_games[['SEASON_ID', 'GAME_ID', 'TEAM_NAME', 'TEAM_ID']]
-        season_game_id_df.loc[:, "PACE"] = np.nan
-        for index, s_g in season_game_id_df.iterrows():
-            out = boxscoreadvancedv2.BoxScoreAdvancedV2(game_id=s_g['GAME_ID'])
+        season_game_id_df.loc[:, "PACE"] = season_game_id_df.apply(lambda row:
+                                                                   get_pace_by_game_id_and_team_id(row['GAME_ID'],
+                                                                                                   row['TEAM_ID']),
+                                                                   axis=1)
 
-            teams_id_1 = out.team_stats.data['data'][0][1]
-            teams_id_2 = out.team_stats.data['data'][1][1]
-            if teams_id_1 == s_g['TEAM_ID']:
-                location = 0
-            elif teams_id_2 == s_g['TEAM_ID']:
-                location = 1
-            else:
-                print('ERROR!')
-            pace = out.team_stats.data['data'][location][25]
-            season_game_id_df.loc[season_game_id_df["GAME_ID"] == s_g['GAME_ID'], 'PACE'] = pace
-            if index == 5:
-                break
         print('hi')
 
 
@@ -52,4 +41,17 @@ def main():
 
 
 if __name__ == '__main__':
-    generate_ds_pace()
+    pd.read_excel('pace__2022_23.xlsx')
+    # from statsmodels.tsa.stattools import grangercausalitytests
+    #
+    # # Generate sample data
+    # data = {'T': [1, 2, 3, 4, 5, 6], 'Y': [2, 3, 4, 5, 6, 7]}
+    # df = pd.DataFrame(data)
+    #
+    # # Perform Granger causality test
+    # granger_test = grangercausalitytests(df[['Y', 'T']], maxlag=2)
+    #
+    # # Print results
+    # for i, test_results in enumerate(granger_test.items()):
+    #     print("\nGranger causality test for lag={}".format(i + 1))
+    #     print("\n".join("{}: {}".format(k, v) for k, v in test_results[1].items()))
